83f3287568701e93527f0a8aed2b1f684703f67d
michaelFarias2022 <makl_2112@hotmail.com.ar>
2024-12-17 13:26:59 -0300

    primer commit

---
+++ claseDeClase.ts
@@ -0,0 +1,35 @@
+class Album{
+    title:string;
+    songs:string[];
+    constructor(title,songs:string[]){
+        this.title=title;
+        this.songs=songs;
+    }
+}
+
+class NombreDeBanda{
+    nombre:string
+}
+
+class Banda{
+    name:NombreDeBanda;
+    members:string[];
+    albums:Album[];
+
+    constructor(members:string[],Albums:Album[]){
+        this.members=members;
+        this.albums=Albums;
+    }
+}
+
+function main(){
+    const unAlbum = {
+        title:"album de marce",
+        songs:["una cancion"],
+    };
+
+    const unaBanda= new Banda(["Marce"],[unAlbum, unAlbum, unAlbum]);
+    console.log(unaBanda.albums);
+
+}
+main();
